7781f79ec9076d234ddf51336a0aba2bf2adf57d
Louis DOUBLECOURT <louis.dbl@hotmail.fr>
2019-11-18 16:48:29 +0100

    Initialisation du dépôt

---
+++ src/main/java/epsi/myAlerts/MyAlertsApplication.java
@@ -0,0 +1,13 @@
+package epsi.myAlerts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAlertsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAlertsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/epsi/myAlerts/MyAlertsApplicationTests.java
@@ -0,0 +1,13 @@
+package epsi.myAlerts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAlertsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
