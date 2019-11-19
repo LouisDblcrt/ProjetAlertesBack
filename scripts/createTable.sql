33aba1eb08985e78a604d603c25a7260bb252cb9
Louis DOUBLECOURT <louis.dbl@hotmail.fr>
2019-11-19 15:58:34 +0100

    [LDT] scripts create table BDD

---
+++ scripts/createTable.sql
@@ -0,0 +1,39 @@
+create table administrator (
+    id int primary key auto_increment,
+    login varchar(150) not null,
+    password varchar(150) not null,
+    name varchar(150) not null,
+    surname varchar(150) not null
+);
+
+create table user(
+id int primary key,
+phone_number varchar(10),
+email varchar(150)
+);
+
+create table alert_owner(
+id int primary key auto_increment,
+enterprise varchar(150) not null,
+description varchar(300),
+priority_max int not null
+);
+
+create table alert(
+id int primary key auto_increment,
+name varchar(100) not null,
+description varchar(300),
+alertDate date not null,
+lieu varchar(50) not null,
+alert_owner int not null,
+constraint FK_ALERT_OWNER foreign key(alert_owner) references alert_owner(id)
+);
+
+create table subscription(
+id_user int,
+id_alert_owner int,
+date_subscription date,
+constraint PK_SUBSCRIPTION primary key(id_user,id_alert_owner),
+constraint FK_ID_USER foreign key(id_user) references user(id),
+constraint FK_ID_ALERT_OWNER foreign key(id_alert_owner) references alert_owner(id)
+);
