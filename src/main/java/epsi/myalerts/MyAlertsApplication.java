561e09e910a3baf2db173439bc4931aa12978aeb
Lucas FRESIERS <fresiers.lucas@gmail.com>
2019-12-05 14:29:19 +0100

    add swagggggggger

--- src/main/java/epsi/myalerts/MyAlertsApplication.java
+++ src/main/java/epsi/myalerts/MyAlertsApplication.java
@@ -5,7 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 @ComponentScan("epsi.myalerts")
 public class MyAlertsApplication {
 
